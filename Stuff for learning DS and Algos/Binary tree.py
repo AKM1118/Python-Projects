e403639215c74935b8396a116fb967f1efaa7c1a
AKM1118 <andredroid16@gmail.com>
2023-03-06 07:59:55 +0500

    Binary tree for testing

---
+++ Stuff for learning DS and Algos/Binary tree.py
@@ -0,0 +1,19 @@
+
+class Node:
+    def __init__(self, value):
+        self.left
+        self.right
+        self.value = value
+class Tree:
+    def __init__(self):
+        self.root = None
+    def getRoot(self):
+        return self.root
+    def addNode(self,value):
+    def _addNode(self,value,node):
+    def findNode(self):
+    def _findNode(self):
+    def printTree(self):
+
+    def deleteTree(self):
+        self.root = None

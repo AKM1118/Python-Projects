16405cdc3035b7d21969d86c5cb5b0070596c370
AKM1118 <andredroid16@gmail.com>
2023-03-10 13:18:49 +0500

    Some reformating

--- Dissertation_project/Dist_Calc_HD.py
+++ Dissertation_project/Dist_Calc_HD.py
@@ -7,18 +7,18 @@ import argparse
 import imutils
 import cv2
 
+
 def midpoint(ptA, ptB):
-	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
-	help="path to the input image")
+                help="path to the input image")
 ap.add_argument("-w", "--width", type=float, required=True,
-	help="width of the left-most object in the image (in cm)")
+                help="width of the left-most object in the image (in cm)")
 args = vars(ap.parse_args())
-
 
 # load the image, convert it to grayscale, and blur it slightly
 image = cv2.imread(args["image"])
@@ -39,98 +39,93 @@ edged = cv2.Canny(gray, 50, 100)
 edged = cv2.dilate(edged, None, iterations=1)
 edged = cv2.erode(edged, None, iterations=1)
 
-
 # find contours in the edge map
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
+                        cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
-
 
 # sort the contours from left-to-right and, then initialize the
 # distance colors and reference object
 (cnts, _) = contours.sort_contours(cnts)
 colors = ((0, 0, 255), (0, 255, 0), (255, 0, 255), (0, 255, 255),
-	(0, 0, 0))
+          (0, 0, 0))
 refObj = None
-
 
 # loop over the contours individually
 for c in cnts:
-	# if the contour is not sufficiently large, ignore it
-	if cv2.contourArea(c) < 4500:
-		continue
-	# compute the rotated bounding box of the contour
-	box = cv2.minAreaRect(c)
-	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-	box = np.array(box, dtype="int")
-	# order the points in the contour such that they appear
-	# in top-left, top-right, bottom-right, and bottom-left
-	# order, then draw the outline of the rotated bounding
-	# box
+    # if the contour is not sufficiently large, ignore it
+    if cv2.contourArea(c) < 4500:
+        continue
+    # compute the rotated bounding box of the contour
+    box = cv2.minAreaRect(c)
+    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+    box = np.array(box, dtype="int")
+    # order the points in the contour such that they appear
+    # in top-left, top-right, bottom-right, and bottom-left
+    # order, then draw the outline of the rotated bounding
+    # box
 
-	box = perspective.order_points(box)
-	#print(box)
-	# compute the center of the bounding box
-	cX = np.average(box[:, 0])
-	cY = np.average(box[:, 1])
+    box = perspective.order_points(box)
+    # print(box)
+    # compute the center of the bounding box
+    cX = np.average(box[:, 0])
+    cY = np.average(box[:, 1])
 
+    # if this is the first contour we are examining (i.e.,
+    # the left-most contour), we presume this is the
+    # reference object
+    if refObj is None:
+        # unpack the ordered bounding box, then compute the
+        # midpoint between the top-left and top-right points,
+        # followed by the midpoint between the top-right and
+        # bottom-right
+        (tl, tr, br, bl) = box
+        (tlblX, tlblY) = midpoint(tl, bl)
+        (trbrX, trbrY) = midpoint(tr, br)
+        # compute the Euclidean distance between the midpoints,
+        # then construct the reference object
+        D = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+        refObj = (box, (cX, cY), D / args["width"])
+        continue
 
-# if this is the first contour we are examining (i.e.,
-	# the left-most contour), we presume this is the
-	# reference object
-	if refObj is None:
-		# unpack the ordered bounding box, then compute the
-		# midpoint between the top-left and top-right points,
-		# followed by the midpoint between the top-right and
-		# bottom-right
-		(tl, tr, br, bl) = box
-		(tlblX, tlblY) = midpoint(tl, bl)
-		(trbrX, trbrY) = midpoint(tr, br)
-		# compute the Euclidean distance between the midpoints,
-		# then construct the reference object
-		D = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-		refObj = (box, (cX, cY), D / args["width"])
-		continue
+    # draw the contours on the image
+    orig = image.copy()
+    cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+    cv2.drawContours(orig, [refObj[0].astype("int")], -1, (0, 255, 0), 2)
+    # stack the reference coordinates and the object coordinates
+    # to include the object center
+    refCoords = np.vstack([refObj[0], refObj[1]])
+    objCoords = np.vstack([box, (cX, cY)])
+    # loop over the original points
+    for ((xA, yA), (xB, yB), color) in zip(refCoords, objCoords, colors):
+        # draw circles corresponding to the current points and
+        # connect them with a line
+        cv2.circle(orig, (int(xA), int(yA)), 5, color, -1)
+        cv2.circle(orig, (int(xB), int(yB)), 5, color, -1)
+        cv2.line(orig, (int(xA), int(yA)), (int(xB), int(yB)),
+                 color, 1)
+        # compute the Euclidean distance between the coordinates,
+        # and then convert the distance in pixels to distance in
+        # units #mX mY
+        D = dist.euclidean((xA, yA), (xB, yB))  # / refObj[2]
+        (mX, mY) = midpoint((xA, yA), (xB, yB))
+        cv2.putText(orig, "{:.4f}cm".format(D), (int(mX), int(mY - 10)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 7, color, 3)
+        # show the output image
+        print(f"comparing ref obj {refObj[0]} and {box}")
+        scale_percent = 15  # percent of original size
+        width = int(orig.shape[1] * scale_percent / 100)
+        height = int(orig.shape[0] * scale_percent / 100)
+        dim = (width, height)
 
-# draw the contours on the image
-	orig = image.copy()
-	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
-	cv2.drawContours(orig, [refObj[0].astype("int")], -1, (0, 255, 0), 2)
-	# stack the reference coordinates and the object coordinates
-	# to include the object center
-	refCoords = np.vstack([refObj[0], refObj[1]])
-	objCoords = np.vstack([box, (cX, cY)])
-	# loop over the original points
-	for ((xA, yA), (xB, yB), color) in zip(refCoords, objCoords, colors):
-		# draw circles corresponding to the current points and
-		# connect them with a line
-		cv2.circle(orig, (int(xA), int(yA)), 5, color, -1)
-		cv2.circle(orig, (int(xB), int(yB)), 5, color, -1)
-		cv2.line(orig, (int(xA), int(yA)), (int(xB), int(yB)),
-				 color, 1)
-		# compute the Euclidean distance between the coordinates,
-		# and then convert the distance in pixels to distance in
-		# units #mX mY
-		D = dist.euclidean((xA, yA), (xB, yB)) / refObj[2]
-		(mX, mY) = midpoint((xA, yA), (xB, yB))
-		cv2.putText(orig, "{:.4f}cm".format(D), (int(mX), int(mY - 10)),
-					cv2.FONT_HERSHEY_SIMPLEX, 7, color, 3)
-		# show the output image
-		print(f"comparing ref obj {refObj[0]} and {box}")
-		scale_percent = 15  # percent of original size
-		width = int(orig.shape[1] * scale_percent / 100)
-		height = int(orig.shape[0] * scale_percent / 100)
-		dim = (width, height)
+        # resize image
+        resized = cv2.resize(orig, dim, interpolation=cv2.INTER_AREA)
 
-		# resize image
-		resized = cv2.resize(orig, dim, interpolation=cv2.INTER_AREA)
+        width = int(orig.shape[1] * scale_percent / 100)
+        height = int(orig.shape[0] * scale_percent / 100)
+        dim = (width, height)
 
-		width = int(orig.shape[1] * scale_percent / 100)
-		height = int(orig.shape[0] * scale_percent / 100)
-		dim = (width, height)
-
-
-		# resize image
-		resizedCanny = cv2.resize(edged, dim, interpolation=cv2.INTER_AREA)
-		cv2.imshow("Image", resized)
-		cv2.waitKey(0)
+        # resize image
+        resizedCanny = cv2.resize(edged, dim, interpolation=cv2.INTER_AREA)
+        cv2.imshow("Image", resized)
+        cv2.waitKey(0)

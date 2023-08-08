211d824955d837fcb939876454025453c218ea6d
AKM1811 <andredroid16@gmail.com>
2023-08-08 20:34:33 +0500

    added undistortion function and cam matrix parameters

--- Dissertation_project/Mark_test.py
+++ Dissertation_project/Mark_test.py
@@ -8,6 +8,11 @@ from imutils import perspective
 from imutils import contours
 from scipy.spatial import distance as dist
 
+ret = 0.7218316116723064
+mtx = [[3.52538591e+03, 0.00000000e+00, 1.64924486e+03], [0.00000000e+00, 3.55117301e+03, 1.16745770e+03],
+       [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]
+distt = numpy.array([-2.77084427e-01, 8.14581003e-01, -1.75902601e-03, 1.31484180e-03, -2.98025019e+00], numpy.float32)
+
 
 def midpoint(ptA, ptB):
     return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
@@ -73,7 +78,8 @@ for c in cnts:
             (box[0][0] - box[3][0]) ** 2 + (box[0][1] - box[3][1]) ** 2)
     #    print("ratio is ", ratio)
 
-    if (0.15 <= ratio <= 0.35) or 0.9 <= ratio <= 1.1:
+    # if (0.15 <= ratio <= 0.35) or 0.9 <= ratio <= 1.1:
+    if 0.9 <= ratio <= 1.1:
         cv2.drawContours(image, [box.astype("int")], -1, (0, 255, 0), 3)
     else:
         continue
@@ -86,24 +92,35 @@ for c in cnts:
         D = dist.euclidean((refContour[0][0], refContour[0][1]), (box[0][0], box[0][1]))
         px_to_cm = D / args["width"]
         orig = image.copy()
-        cv2.circle(orig, (int(refContour[0][0]), int(refContour[0][1])), 5, (255,0,0), -1)
-        cv2.circle(orig, (int(box[0][0]), int(box[0][1])), 5, (255,0,0), -1)
+        cv2.circle(orig, (int(refContour[0][0]), int(refContour[0][1])), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(box[0][0]), int(box[0][1])), 5, (255, 0, 0), -1)
         cv2.line(orig, (int(refContour[0][0]), int(refContour[0][1])), (int(box[0][0]), int(box[0][1])),
-                 (255,0,0), 5)
+                 (255, 0, 0), 5)
         (mX, mY) = midpoint((refContour[0][0], refContour[0][1]), (box[0][0], box[0][1]))
         cv2.putText(orig, "{:.4f}".format(D), (int(mX), int(mY - 10)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 6, (255,0,0), 5)
+                    cv2.FONT_HERSHEY_SIMPLEX, 6, (255, 0, 0), 5)
         print(px_to_cm)
-        width = int(orig.shape[1] * 15 / 100)
-        height = int(orig.shape[0] * 15 / 100)
+        width = int(orig.shape[1] * 60 / 100)
+        height = int(orig.shape[0] * 60 / 100)
         dim = (width, height)
         resized = cv2.resize(orig, dim, interpolation=cv2.INTER_AREA)
         cv2.imshow("Image", resized)
         cv2.waitKey(0)
     cnts_mark.append(box)
 px = True
+orig = image.copy()
+h, w = orig.shape[:2]
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, distt, (w, h), 1, (w, h))
+dst = cv2.undistort(orig, mtx, distt, None, newcameramtx)
+scale_percent = 60  # percent of original size
+width = int(dst.shape[1] * scale_percent / 100)
+height = int(dst.shape[0] * scale_percent / 100)
+dim = (width, height)
+resized = cv2.resize(dst, dim, interpolation=cv2.INTER_AREA)
+cv2.imshow("Image", resized)
+cv2.waitKey(0)
 for c in cnts_mark:
-    #if px:
+    # if px:
     #    D = dist.euclidean((c[0][0], c[0][1]), (c[1][0], c[1][1]))
     #    px_to_cm = D / args["width"]
     #    px = False
@@ -117,24 +134,35 @@ for c in cnts_mark:
             xA, yA, xB, yB = c[i][0], c[i][1], c[i + 1][0], c[i + 1][1]
         # draw circles corresponding to the current points and
         # connect them with a line
-        cv2.circle(orig, (int(xA), int(yA)), 5, color, -1)
-        cv2.circle(orig, (int(xB), int(yB)), 5, color, -1)
-        cv2.line(orig, (int(xA), int(yA)), (int(xB), int(yB)),
+        # cv2.circle(orig, (int(xA), int(yA)), 5, color, -1)
+        ##cv2.circle(orig, (int(xB), int(yB)), 5, color, -1)
+        # cv2.line(orig, (int(xA), int(yA)), (int(xB), int(yB)),
+        #         color, 3)
+        cv2.circle(dst, (int(xA), int(yA)), 5, color, -1)
+        cv2.circle(dst, (int(xB), int(yB)), 5, color, -1)
+        cv2.line(dst, (int(xA), int(yA)), (int(xB), int(yB)),
                  color, 3)
         # compute the Euclidean distance between the coordinates,
         # and then convert the distance in pixels to distance in
         # units #mX mY
         D = dist.euclidean((xA, yA), (xB, yB)) / px_to_cm
         (mX, mY) = midpoint((xA, yA), (xB, yB))
-        cv2.putText(orig, "{:.4f}cm".format(D), (int(mX), int(mY - 10)),
+        # cv2.putText(orig, "{:.4f}cm".format(D), (int(mX), int(mY - 10)),
+        #            cv2.FONT_HERSHEY_SIMPLEX, 7, color, 3)
+        cv2.putText(dst, "{:.4f}cm".format(D), (int(mX), int(mY - 10)),
                     cv2.FONT_HERSHEY_SIMPLEX, 7, color, 3)
         length_box.append(D)
 
-        scale_percent = 15  # percent of original size
-        width = int(orig.shape[1] * scale_percent / 100)
-        height = int(orig.shape[0] * scale_percent / 100)
+        # scale_percent = 60    # percent of original size
+        # width = int(orig.shape[1] * scale_percent / 100)
+        # height = int(orig.shape[0] * scale_percent / 100)
+        # dim = (width, height)
+        # resized = cv2.resize(orig, dim, interpolation=cv2.INTER_AREA)
+        scale_percent = 60  # percent of original size
+        width = int(dst.shape[1] * scale_percent / 100)
+        height = int(dst.shape[0] * scale_percent / 100)
         dim = (width, height)
-        resized = cv2.resize(orig, dim, interpolation=cv2.INTER_AREA)
+        resized = cv2.resize(dst, dim, interpolation=cv2.INTER_AREA)
         cv2.imshow("Image", resized)
         cv2.waitKey(0)
         i += 1

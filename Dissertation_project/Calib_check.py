Fixed deviation calculation for x,y coordinates after undistortion

Dissertation_project/Calib_check.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 from scipy.spatial import distance
 img = cv2.imread('SDC12887.JPG')
-
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 r, c = 6, 9
 
 sub_corner_points = np.array([[1344.6705, 1444.8865], [1339.8512, 1372.2052], [1335.1322, 1298.0023], [1330.5327, 1223.1202],
@@ -102,18 +102,20 @@ imgpoints2, _ = cv2.projectPoints(object_points, rvecs, tvecs, mtx, dist)
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret, corners = cv2.findChessboardCorners(gray, (6, 9), None)
+
 if ret:
  cv2.drawChessboardCorners(img, (6, 9), corners, ret)
  for i in range(len(corners)):
   coord = corners[i]
   x_old = coord[0,0]
   y_old = coord[0,1]
-  cv2.circle(img, (int(x_old), int(y_old)), 8, (175+i,39+i, 45+i), -1)
+  cv2.circle(img, (int(x_old), int(y_old)), 1, (175+i,39+i, 45+i), -1)
   cv2.putText(img, "{}".format(i),(int(x_old - 50), int(y_old - 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (39+i, 172+i, 45+i), 2)
 
 
 gray = cv2.cvtColor(undist,cv2.COLOR_BGR2GRAY)
 ret, corners = cv2.findChessboardCorners(gray, (6, 9), None)
+corners_sub_pixel = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
 print("###### drawing points on a new image ######")
 if ret:
  cv2.drawChessboardCorners(undist, (6, 9), corners, ret)
@@ -122,20 +124,25 @@ if ret:
   x_new = coord[0,0]
   y_new = coord[0,1]
   # cv2.circle(undist, (int(x), int(y)), 8, (175+i,39+i, 45+i), -1)
-  cv2.circle(undist, (int(x_new), int(y_new)), 8, (128 + i, 128 + i, 128 + i), -1)
-  cv2.circle(img, (int(x_new), int(y_new)), 8, (128 + i, 128 + i, 128 + i), -1)
+  cv2.circle(undist, (int(x_new), int(y_new)), 1, (128 + i, 128 + i, 128 + i), -1)
+  cv2.circle(img, (int(x_new), int(y_new)), 1, (128 + i, 128 + i, 128 + i), -1)
   cv2.putText(undist, "{}".format(i),(int(x_new - 50), int(y_new - 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (45+i, 39+i, 175+i), 2)
+
+  coord = corners_sub_pixel[i]
+  x_new = coord[0, 0]
+  y_new = coord[0, 1]
+  cv2.circle(undist, (int(x_new), int(y_new)), 1, (168 + i, 55 + i, 12 + i), -1)
 print("############### writing cal2 image #############")
 cv2.imwrite('calbka_2.png', undist)
 cv2.imwrite('calbka_1.png', img)
 print("###### calculating x points for undist ######")
 for i in range(c):
- coord = corners[0+r*(i-1)]
+ coord = corners_sub_pixel[0+r*(i-1)]
  x_st, y_st = coord[0,0], coord[0,1]
- coord = corners[5+r*(i-1)]
+ coord = corners_sub_pixel[5+r*(i-1)]
  x_end, y_end = coord[0,0], coord[0,1]
  for j in range(r-1):
-  coord = corners[j+r*(i-1)]
+  coord = corners_sub_pixel[j+r*(i-1)]
   xi , yi = coord[0,0], coord[0,1]
   xi_calc = (yi-y_st)*(x_end-x_st)/(y_end-y_st)+x_st
   x_diff = xi - xi_calc
@@ -144,12 +151,12 @@ for i in range(c):
 print(x_list1)
 print("###### calculating y points for undist ######")
 for i in range(r):
- coord = corners[i-1]
+ coord = corners_sub_pixel[i-1]
  x_st, y_st = coord[0,0], coord[0,1]
- coord = corners[48+i-1]
+ coord = corners_sub_pixel[48+i-1]
  x_end, y_end = coord[0,0], coord[0,1]
  for j in range(c-1):
-  coord = corners[6+r*(j-1)+(i-1)]
+  coord = corners_sub_pixel[6+r*(j-1)+(i-1)]
   xi , yi = coord[0,0], coord[0,1]
   yi_calc = (xi-x_st)*(y_end-y_st)/(x_end-x_st)+y_st
   y_diff = yi - yi_calc
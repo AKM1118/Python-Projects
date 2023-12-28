1818856099bab309b4493cea919caa39c51494a8
AKM1118 <andredroid16@gmail.com>
2023-12-28 13:06:24 +0500

    Refactored the code. Created a wrapper to count execution time. Most of the operations are moved to functions

--- Dissertation_project/cam_cal.py
+++ Dissertation_project/cam_cal.py
@@ -3,10 +3,24 @@ import numpy as np
 import cv2
 import glob
 import math
+import time
+from functools import wraps
 #import Main
 
 #print(Main.a)
-def draw(img, corners, imgpts, xAng, yAng, zAng):
+def timeToComplete(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print((f"{func} is in process"))
+        time_start = time.time()
+        val = func(*args, **kwargs)
+        time_end = time.time() - time_start
+        print((f"{func.__name__} is done in {time_end} s"))
+        return val
+    return wrapper
+@timeToComplete
+def draw(image, corners, imgpts, xAng, yAng, zAng):
+ img = cv2.imread(image)
  corner = tuple(corners[0].ravel())
  imgx = tuple(imgpts[0].ravel())
  imgy = tuple(imgpts[1].ravel())
@@ -20,130 +34,138 @@ def draw(img, corners, imgpts, xAng, yAng, zAng):
              cv2.FONT_HERSHEY_SIMPLEX,3 , (0, 255, 0), 5)
  cv2.putText(img, "yAng {:.4f}".format(yAng), ((int(imgy[0]),int(imgy[1]-10))),
              cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 5)
- #cv2.putText(img, "zAng {:.4f}".format(zAng), ((int(imgz[0]),int(imgz[1]-10))),
- #            cv2.FONT_HERSHEY_SIMPLEX, 6, (0, 0, 255), 5)
+ cv2.putText(img, "zAng {:.4f}".format(zAng), ((int(imgz[0]),int(imgz[1]+150))),
+             cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 5)
  return img
-
+@timeToComplete
 def resizeImage(img, scale):
-    img_pre_resize = img.copy()
-    width = int(img_pre_resize.shape[1] * scale / 100)
-    height = int(img_pre_resize.shape[0] * scale / 100)
+    width = int(img.shape[1] * scale / 100)
+    height = int(img.shape[0] * scale / 100)
     dimensions = (width, height)
-    resized_image = cv2.resize(img_pre_resize, dimensions, interpolation=cv2.INTER_AREA)
+    resized_image = cv2.resize(img, dimensions, interpolation=cv2.INTER_AREA)
     return resized_image
+
 def showMyImage(img, scale):
     img_show = img.copy()
     cv2.imshow("Image", resizeImage(img_show,scale))
     cv2.waitKey(0)
-a = None
-a = np.load('cam_param.npz')
-mtx= a['mtx']
-dist = a['dist']
-print(f"mtx = {mtx} # "
-      f"dist = {dist} # ")
-# criteria for sub pixel corner detection
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# creating arrays to store all found corner points
-object_points = np.zeros((6*9, 3), np.float32)
-object_points[:, :2] = np.mgrid[0:6, 0:9].T.reshape(-1, 2)
-axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
-# arrays where we store points from processed images
-points_on_object = []
-points_on_image = []
-# find all images used for calibration
-images = glob.glob('*.jpg')
-image_test = glob.glob('Dist_detect_7/*.JPG')
-# perform corner point extraction for every image found in the folder
-if a is None:
-    for fname in images:
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray, (6, 9), None)
+@timeToComplete
+def getRecalibError(obj_points,img_points,rvecs,tvecs,mtx,dist):
+    mean_error = 0
+    for i in range(len(obj_points)):
+        imgpoints2, _ = cv2.projectPoints(obj_points[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(img_points[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+        mean_error += error
+        print("total error: {}".format(mean_error / len(obj_points)))
+
+@timeToComplete
+def getInnerParam(calibration_images=list[str],board_x=int,board_y=int,save_path=str):
+    j = 0
+    for image in calibration_images:
+        print(image)
+        j += 1
+        img = cv2.imread(image)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (board_y, board_x), None)
+        print(f"board {j} is {ret}")
         if ret:
             points_on_object.append(object_points)
             corners_sub_pixel = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             points_on_image.append(corners_sub_pixel)
-            cv2.drawChessboardCorners(img, (6, 9), corners, ret)
-            for i in range(len(corners_sub_pixel)):
-                coord = corners_sub_pixel[i]
-                x = coord[0,0]
-                y = coord[0,1]
-                #cv2.circle(img, (int(x), int(y)), 8, (175+i,39+i, 45+i), -1)
-                #cv2.putText(img, "{}".format(i),
-                #            (int(x - 50), int(y - 20)), cv2.FONT_HERSHEY_SIMPLEX,
-                #            0.8, (175+i, 39+i, 45+i), 2)
-            #for i in range(len(corners_sub_pixel)):
-            #    y = (corners_sub_pixel[i,0]-corners_sub_pixel[i,0])
-            #for i in range(len(corners_sub_pixel) / 6):
-            #    x = (corners_sub_pixel[i, 0] - corners_sub_pixel[i, 0])
+            cv2.drawChessboardCorners(img, (board_y, board_x), corners, ret)
 
-        #scale_percent = 55  # percent of original size
-        #width = int(img.shape[1] * scale_percent / 100)
-        #height = int(img.shape[0] * scale_percent / 100)
-        #dim = (width, height)
-        #resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-        #cv2.imshow('img', resized)
-        #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-# extracting calibration parameters from calibration images
-    ret,mtx,dist,rvecs,tvecs = cv2.calibrateCamera(points_on_object,points_on_image, gray.shape[::-1], None, None)
-    np.savez('cam_param', mtx=mtx, dist=dist)
+    # extracting calibration parameters from calibration images
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(points_on_object, points_on_image, gray.shape[::-1], None, None)
+    np.savez(save_path, mtx=mtx, dist=dist)
     print(f"mtx = {mtx} # "
-      f"dist = {dist} # ")
-    #################################################################
-    # using said parameters to undistort an unrelated image
-    img = cv2.imread('SDC12902.JPeG')
-    h, w = img.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-    cv2.imwrite('calibresult2.png', dst)
-    mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
-    dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
-    cv2.imwrite('calibresult3.png', dst)
-# crop the image
-# x, y, w, h = roi
-# dst = dst[y:y+h, x:x+w]
-# image = cv2.hconcat([img, dst])
-    cv2.imwrite('calibresult1.png', img)
-    mean_error = 0
-    for i in range(len(points_on_object)):
-        imgpoints2, _ = cv2.projectPoints(points_on_object[i], rvecs[i], tvecs[i], mtx, dist)
-        error = cv2.norm(points_on_image[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-        mean_error += error
-        print( "total error: {}".format(mean_error/len(points_on_object)) )
-        print(type(ret),type(mtx),type(dist),type(rvecs),type(tvecs))
+          f"dist = {dist} # ")
+    getRecalibError(points_on_object,points_on_image,rvecs,tvecs,mtx,dist)
 
-object_points_test = np.zeros((4*7, 3), np.float32)
-object_points_test[:, :2] = np.mgrid[0:4, 0:7].T.reshape(-1, 2)
-for frame in image_test:
- img = cv2.imread(frame)
- cp = img.copy()
- gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
- ret, corners = cv2.findChessboardCorners(gray, (4,7),None)
- print(ret)
- if ret == True:
-    corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-    # cv2.drawChessboardCorners(cp, (4, 9), corners, ret)
-    # Find the rotation and translation vectors.
-    ret,rvecs, tvecs = cv2.solvePnP(object_points_test, corners2, mtx, dist)
-
-    # project 3D points to image plane
-    imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
-
+@timeToComplete
+def getAngles(rvecs):
     Rmtx = cv2.Rodrigues(rvecs)
-    print(f"tvecs is {tvecs}, rvecs is {Rmtx}")
-    print(Rmtx[0])
+    # print(Rmtx[0])
     sy = math.sqrt(Rmtx[0][0][0] * Rmtx[0][0][0] + Rmtx[0][1][0] * Rmtx[0][1][0])
     xAng = math.degrees(math.atan2(Rmtx[0][2][1], Rmtx[0][2][2]))
     yAng = math.degrees(math.atan2(-Rmtx[0][2][0], sy))
     zAng = math.degrees(math.atan2(Rmtx[0][1][0], Rmtx[0][0][0]))
-    print(f"xAng = {xAng}, yAng = {yAng}, zAng = {zAng}")
-    img = draw(img,corners2,imgpts,xAng,yAng,zAng)
-    showMyImage(img,30)
-    #showMyImage(cp, 30)
-cv2.destroyAllWindows()
+    print(f"xAng = {xAng} type {type(xAng)}, yAng = {yAng} type {type(yAng)}, zAng = {zAng} type {type(zAng)}")
+    return xAng, yAng, zAng
+@timeToComplete
+def getCorners(image,board_x,board_y):
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (board_y, board_x), None)
+    print(ret)
+    return ret,corners, gray
+def main():
+    try:
+        cam_params = np.load(calib_param_path)
+    except OSError:
+        print('No inner parameters found, trying to calculate new ones...')
+        calib_images = glob.glob(calib_img_path)
+        if calib_images == []:
+            raise Exception('No calibration images found, please choose a different folder')
+        getInnerParam(calib_images,board_x_cal,board_y_cal,save_path)
+        cam_params = np.load(calib_param_path)
+
+    mtx = cam_params['mtx']
+    dist = cam_params['dist']
+    print(f"mtx = {mtx} # "
+          f"dist = {dist} # ")
+
+    image_test = glob.glob(detect_img_path)
+    object_points = np.zeros((board_y_detect * board_x_detect, 3), np.float32)
+    object_points[:, :2] = np.mgrid[0:board_y_detect, 0:board_x_detect].T.reshape(-1, 2)
+
+    for frame in image_test:
+        ret, corners, gray = getCorners(frame,board_x_detect,board_y_detect)
+        img = cv2.imread(frame)
+        if ret == True:
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
+            # Find the rotation and translation vectors.
+            ret, rvecs, tvecs = cv2.solvePnP(object_points, corners2, mtx, dist)
+
+            # project 3D points to image plane
+            imgpts, _ = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+            xAng, yAng, zAng = getAngles(rvecs)
+            img = draw(frame, corners2, imgpts, xAng, yAng, zAng)
+            showMyImage(img, 30)
+            # showMyImage(cp, 30)
+    cv2.destroyAllWindows()
+
+# Constants
+
+# board size for x and y
+board_x_cal = 10
+board_y_cal = 7
+
+board_x_detect = 3
+board_y_detect = 7
+# criteria for sub pixel corner detection
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# creating arrays to store all found corner points
+object_points = np.zeros((board_y_cal*board_x_cal, 3), np.float32)
+object_points[:, :2] = np.mgrid[0:board_y_cal, 0:board_x_cal].T.reshape(-1, 2)
+axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+
+# arrays where we store points from processed images
+points_on_object = []
+points_on_image = []
+
+# file paths for calibration and detection
+calib_img_path = 'Cam_calib_1/*.JPG'
+calib_param_path = 'cam_param.npz'
+detect_img_path = 'Dist_detect_4/*.JPG'
+save_path = 'cam_param'
+
+# Get inner parameters if there are none
 
 
+if __name__ == "__main__":
+    main()
 
 
 """
